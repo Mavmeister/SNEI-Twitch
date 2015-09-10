7a641397ee33cc5abf4593ae9fb9e010d2ee1158
James Maveety <jimbobthedrummer@gmail.com>
2015-09-10 14:21:07 -0700

    [feat] Add pagination, hyperlinking of stream

--- app.js
+++ app.js
@@ -13,15 +13,33 @@ var searchJSONP = function(query){
   document.getElementsByTagName("head")[0].removeChild(scriptTag);
 };
 
+var navigatePage = function(link){
+  if (!link){
+    return;
+  }
+  console.log('link', link)
+  var scriptTag = document.createElement('script');
+  var searchQuery = link + "&callback=buildResults";
+  scriptTag.setAttribute("src", searchQuery);
+  document.getElementsByTagName("head")[0].appendChild(scriptTag);
+  document.getElementsByTagName("head")[0].removeChild(scriptTag);
+}
+
 var buildResults = function(json){
   if (json.streams.length === 0){
-    return document.getElementById('result-container').innerHTML = "No Search Results Available"
+    return document.getElementById('result-container').innerHTML = "No Search Results Available";
   }
   console.log('Streams', json);
-  var channels = [];
   // Parse each channel and create an object with desired properties
+  var channels = [];
+  var nextPageLink =  json._links.next;
+  var prevPageLink =  json._links.prev  || null;
+  var currentPage = 1; 
+  var maxPage = Math.ceil(json._total / 10);
+
   eachResults(json.streams, function(stream){
-    channelContents = "<div id='stream-contents'><img id='stream-image' src='"
+    channelContents = "<a id='tag' href="
+     + stream.channel.url + "><div id='stream-contents'><img id='stream-image' src='"
      + stream.preview.medium + " '><span id='stream-name'>"
      + stream.channel.status + "</span></br><span id='stream-game'>"
      + stream.channel.game + "</span> - <span id='stream-viewers'>"
@@ -29,17 +47,16 @@ var buildResults = function(json){
      + stream.channel.name+ "</span></div>";
     channels.push(channelContents);
   });
-    channelCount = "<span id='stream-count'>Total results: " + (json._total - 1) + "</span>";
+    channelCount = "<span id='stream-count'>Total results: " + json._total + "</span>";
+    nextPage = "<span id='next-page' onclick=navigatePage(" + nextPageLink + ")> NEXT " + maxPage + " </span>";
+    prevPage = "<span id='prev-page' onclick=navigatePage(" + prevPageLink + ")>" + currentPage + " PREV </span>";
+
     // REFACTOR: add count and result in one operation to container div
     document.getElementById('count-container').innerHTML = channelCount;
-    document.getElementById('page-container').innerHTML = '< 1 out of 10 >'
+    document.getElementById('page-container').innerHTML = prevPage + nextPage;
     document.getElementById('result-container').innerHTML = channels.join('');
 };
 
-var nextPage = function(channel){
-  var channelElement = document.createElement('div');
-  channelElement.appendChild(channelContents)
-}
 
 // HELPER FUNCTIONS
 var eachResults = function(collection, iterator){
