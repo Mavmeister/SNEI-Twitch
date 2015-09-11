ecf569394b39783a3dd398f546a6f2cd38fd73d3
James Maveety <jimbobthedrummer@gmail.com>
2015-09-11 14:08:47 -0700

    [cleanup] Code cleanup for submission

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 var currentPage = 1; 
 
 // SEARCH AND DATA FUNCTIONS
+
+// Handles listening for ENTER keypress to search
 var handleKey = function(event, value){
   if (event.keyCode === 13){
     searchJSONP(value)
@@ -9,9 +11,9 @@ var handleKey = function(event, value){
   return;
 };
 
+// Query function using JSONP and callback for CORS workaround
 var searchJSONP = function(query){
   var scriptTag = document.createElement('script');
-  // Creating a JSONP callback for CORS workaround
   var searchQuery = "https://api.twitch.tv/kraken/search/streams?q=" + query + "&callback=buildResults";
   scriptTag.setAttribute("src", searchQuery);
   document.getElementById('search-input').value = ''
@@ -20,6 +22,7 @@ var searchJSONP = function(query){
   document.getElementsByTagName("head")[0].removeChild(scriptTag);
 };
 
+// Navigation function for querying next/prev search links
 var navigatePage = function(link, direction){
   console.log(direction)
   if (link === 'null'){
@@ -33,32 +36,34 @@ var navigatePage = function(link, direction){
   document.getElementsByTagName("head")[0].removeChild(scriptTag);
 };
 
+// Builds all results on the page after a successful search
 var buildResults = function(json){
+  // Clear results/page count to avoid remnants of past search when No Results appear
+    document.getElementById('count-container').innerHTML = '';
+    document.getElementById('page-container').innerHTML = '';
   if (json.streams.length === 0){
-    return document.getElementById('result-container').innerHTML = "No Search Results Available";
+    return document.getElementById('result-container').innerHTML = "<span id='no-results'> No Search Results Available </span>";
   }
-  console.log('Streams', json);
-  // Parse each channel and create an object with desired properties
   var channels = [];
   var nextPageLink =  json._links.next;
   var prevPageLink =  json._links.prev || null;
   var maxPage = Math.ceil(json._total / 10);
 
+  // Parse each channel and create an object with desired properties
   json.streams.forEach(function(stream){
-    channelContents = "<a id='tag' href="
+    channelContents = "<a id='contents' href="
      + stream.channel.url + "><div id='stream-contents'><img id='stream-image' src='"
      + stream.preview.medium + " '><span id='stream-name'>"
      + stream.channel.status + "</span></br><span id='stream-game'>"
      + stream.channel.game + "</span> - <span id='stream-viewers'>"
-     + stream.viewers + " viewers</span><br/><span id='stream-desc'>"
+     + stream.viewers + " viewers</span><br/><span id='stream-desc'> Streamed By: "
      + stream.channel.name+ "</span></div>";
     channels.push(channelContents);
   });
+  // Adds dynamic data to results page (results, count, pagination)
     channelCount = "<span id='stream-count'>Total results: " + json._total + "</span>";
-    nextPage = "<span id='next-page' onclick=navigatePage('" + nextPageLink + "','next')> NEXT " + maxPage + ' &rArr;' + " </span>";
-    prevPage = "<span id='prev-page' onclick=navigatePage('" + prevPageLink + "','prev')>" + '&lArr; ' + 1 + " PREV </span>";
-
-    // REFACTOR: add count and result in one operation to container div
+    nextPage = "<span id='next-page' onclick=navigatePage('" + nextPageLink + "','next')> NEXT " + maxPage + ' &#8702;' + " </span>";
+    prevPage = "<span id='prev-page' onclick=navigatePage('" + prevPageLink + "','prev')>" + '&#8701; ' + 1 + " PREV </span>";
     document.getElementById('count-container').innerHTML = channelCount;
     document.getElementById('page-container').innerHTML = prevPage + "<span id='current-page'>"+currentPage+"</span>" + nextPage;
     document.getElementById('result-container').innerHTML = channels.join('');
